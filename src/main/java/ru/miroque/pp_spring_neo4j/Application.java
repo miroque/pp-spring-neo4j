770923e0bb9c8a99b37acf85d32e434aeb62bb7c
Panov Aleksander <a.panov@sysolutions.ru>
2019-12-02 16:36:24 +0700

    the same with rest

--- src/main/java/ru/miroque/pp_spring_neo4j/Application.java
+++ src/main/java/ru/miroque/pp_spring_neo4j/Application.java
@@ -1,16 +1,12 @@
 package ru.miroque.pp_spring_neo4j;
-import java.util.Arrays;
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @SpringBootApplication
 @EnableNeo4jRepositories
 public class Application {
@@ -21,42 +17,42 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Bean
-	CommandLineRunner demo(PersonRepository personRepository) {
-		return args -> {
-
-			personRepository.deleteAll();
-
-			Person greg = new Person("Greg");
-			Person roy = new Person("Roy");
-			Person craig = new Person("Craig");
-
-			List<Person> team = Arrays.asList(greg, roy, craig);
-
-			log.info("Before linking up with Neo4j...");
-
-			team.stream().forEach(person -> log.info("\t" + person.toString()));
-
-			personRepository.save(greg);
-			personRepository.save(roy);
-			personRepository.save(craig);
-
-			greg = personRepository.findByName(greg.getName());
-			greg.worksWith(roy);
-			greg.worksWith(craig);
-			personRepository.save(greg);
-
-			roy = personRepository.findByName(roy.getName());
-			roy.worksWith(craig);
-			// We already know that roy works with greg
-			personRepository.save(roy);
-
-			// We already know craig works with roy and greg
-
-			log.info("Lookup each person by name...");
-			team.stream().forEach(person -> log.info(
-					"\t" + personRepository.findByName(person.getName()).toString()));
-		};
-	}
+//	@Bean
+//	CommandLineRunner demo(PersonRepository personRepository) {
+//		return args -> {
+//
+//			personRepository.deleteAll();
+//
+//			Person greg = new Person("Greg");
+//			Person roy = new Person("Roy");
+//			Person craig = new Person("Craig");
+//
+//			List<Person> team = Arrays.asList(greg, roy, craig);
+//
+//			log.info("Before linking up with Neo4j...");
+//
+//			team.stream().forEach(person -> log.info("\t" + person.toString()));
+//
+//			personRepository.save(greg);
+//			personRepository.save(roy);
+//			personRepository.save(craig);
+//
+//			greg = personRepository.findByName(greg.getName());
+//			greg.worksWith(roy);
+//			greg.worksWith(craig);
+//			personRepository.save(greg);
+//
+//			roy = personRepository.findByName(roy.getName());
+//			roy.worksWith(craig);
+//			// We already know that roy works with greg
+//			personRepository.save(roy);
+//
+//			// We already know craig works with roy and greg
+//
+//			log.info("Lookup each person by name...");
+//			team.stream().forEach(person -> log.info(
+//					"\t" + personRepository.findByName(person.getName()).toString()));
+//		};
+//	}
 
 }
--- src/main/java/ru/miroque/pp_spring_neo4j/PersonRepository.java
+++ src/main/java/ru/miroque/pp_spring_neo4j/PersonRepository.java
@@ -1,7 +1,9 @@
 package ru.miroque.pp_spring_neo4j; 
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
 public interface PersonRepository extends CrudRepository<Person, Long> {
 
 	Person findByName(String name);
