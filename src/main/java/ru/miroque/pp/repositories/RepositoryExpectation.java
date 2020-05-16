8f1a56b059dce28fafee45ce89622b3e7aee1090
Panov Aleksander <a.panov@sysolutions.ru>
2020-05-16 11:54:13 +0700

    :construction: added docker-compose file

--- src/main/java/ru/miroque/pp/repositories/RepositoryExpectation.java
+++ src/main/java/ru/miroque/pp/repositories/RepositoryExpectation.java
@@ -3,9 +3,9 @@ package ru.miroque.pp.repositories;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Repository;
 import ru.miroque.pp.nodes.Expectation;
-import ru.miroque.pp.nodes.Knowledge;
 
 @Repository
 public interface RepositoryExpectation extends Neo4jRepository<Expectation, Long> {
-
+    @Override
+    Iterable<Expectation> findAll();
 }
